8fd04dca29965fad8f7c92c9b19757c6c21a16cc
Venkatasubramanyam Perepu <vperepu@crestron.com>
2020-07-25 10:09:07 +0530

    added Enum helper class

---
+++ PromotionEngine/PromotionEngine/Helpers/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Helpers
+{
+    public enum PromotionTypes
+    {
+        NItemsSKU = 1,
+        SKU1andSKU2 = 2
+    }
+}
