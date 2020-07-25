a148e0e4464ea946589f146dfbc2c5d0b04c9a59
Venkatasubramanyam Perepu <vperepu@crestron.com>
2020-07-25 10:13:37 +0530

    added individual promotion type calsses inherited form IPromotion TYpe interface

---
+++ PromotionEngine/PromotionEngine/NItemsSKU.cs
@@ -0,0 +1,17 @@
+﻿using PromotionEngine.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class NItemsSKU : IPromotionType
+    {
+        public decimal CaliculatePromotionValue(List<char> SKUs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PromotionEngine/PromotionEngine/PromotionTypeFactory.cs
@@ -0,0 +1,25 @@
+﻿using PromotionEngine.Helpers;
+using PromotionEngine.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    public class PromotionTypeFactory
+    {
+        static IPromotionType GetPromotion(int promotionType)
+        {
+            IPromotionType objPromotionType;
+            switch (promotionType)
+            {
+                case (int)PromotionTypes.NItemsSKU: objPromotionType = new NItemsSKU(); break;
+                case (int)PromotionTypes.SKU1andSKU2: objPromotionType = new SKU1andSKU2(); break;
+                default: objPromotionType = null; break;
+            }
+            return objPromotionType;
+        }
+    }
+}
