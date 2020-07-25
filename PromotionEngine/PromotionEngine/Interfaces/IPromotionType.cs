8af2a784210880cbd1a7404c5e03b26b536451ab
Venkatasubramanyam Perepu <vperepu@crestron.com>
2020-07-25 10:07:42 +0530

    added IPromotionType

---
+++ PromotionEngine/PromotionEngine/Interfaces/IPromotionType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Interfaces
+{
+    public interface IPromotionType
+    {
+        decimal CaliculatePromotionValue(List<char> SKUs);
+    }
+}
